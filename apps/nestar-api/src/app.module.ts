cb7b9eb55237426366276b87459de179526470ed
SUNNATULLO <sunnatullokenjaev8@gmail.com>
2025-09-16 06:38:41 +0900

    feat: create components related modules

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -9,7 +9,8 @@ import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({
+  imports: [ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
     driver: ApolloDriver,
     playground: true,
     uploads: false,
