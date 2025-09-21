39bedb1c7aca8acc0523b8355f019aa9bcc48a2c
SUNNATULLO <sunnatullokenjaev8@gmail.com>
2025-09-21 12:24:12 +0900

    feat: hashing password via bcryptjs

--- apps/train.ts
+++ apps/train.ts
@@ -14,9 +14,36 @@
 // printNumbers();
 
 // TASK ZM
-function reverseInteger(num) {
-	const reversed = num.toString().split('').reverse().join('');
-	return parseInt(reversed, 10);
+// function reverseInteger(num) {
+// 	const reversed = num.toString().split('').reverse().join('');
+// 	return parseInt(reversed, 10);
+// }
+
+// console.log(reverseInteger(123456789));
+
+// TASK ZL
+function stringToKebab(str: string) {
+	return str
+		.toLowerCase()
+		.replace(/[^a-z0-9\s]/g, '')
+		.trim()
+		.replace(/\s+/g, '-');
+}
+console.log(stringToKebab('I love Kebab'));
+
+console.log('***************');
+
+// TEST
+function stringToKebab1(str: string) {
+	return str
+		.toLowerCase()
+		.replace(/[^\p{L}\p{N}\s-]/gu, '')
+		.trim()
+		.replace(/[\s_-]+/g, '-')
+		.replace(/^-+|-+$/g, '');
 }
 
-console.log(reverseInteger(123456789));
+console.log(stringToKebab1('I love Kebab'));
+console.log(stringToKebab1('Hello World Im Toby!'));
+console.log(stringToKebab1('데벡스 아카데미 최고야!'));
+console.log(stringToKebab1('Девекс Академия самая классная!'));
