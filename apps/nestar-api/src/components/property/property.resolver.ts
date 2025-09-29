3fd3f0106c914dfe053ef0a9542c2bd901f7548c
SUNNATULLO <sunnatullokenjaev8@gmail.com>
2025-09-29 23:28:14 +0900

    feat: develop getProperties GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,7 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
-import { Property } from '../../libs/dto/property/property';
-import { PropertyInput } from '../../libs/dto/property/property.input';
+import { Properties, Property } from '../../libs/dto/property/property';
+import { PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
@@ -53,5 +53,16 @@ export class PropertyResolver {
 		console.log('Mutation: updateProperty');
 		input._id = shapeIntoMogoObjectId(input._id);
 		return await this.propertyService.updateProperty(memberId, input);
+	}
+
+	// getProperties
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Properties)
+	public async getProperties(
+		@Args('input') input: PropertiesInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getProperties');
+		return await this.propertyService.getProperties(memberId, input);
 	}
 }
