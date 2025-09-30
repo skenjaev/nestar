3d3f4842d0ecb017e634d58b99c0383c4d9aac9a
SUNNATULLO <sunnatullokenjaev8@gmail.com>
2025-09-30 19:02:20 +0900

    feat: develop updatePropertyByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -84,7 +84,7 @@ export class PropertyResolver {
 	}
 
 	/** ADMIN **/
-	//getAllPropertiesByAdmin
+	// getAllPropertiesByAdmin
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
 	@Query((returns) => Properties)
@@ -94,5 +94,14 @@ export class PropertyResolver {
 	): Promise<Properties> {
 		console.log('Query: getAllPropertiesByAdmin');
 		return await this.propertyService.getAllPropertiesByAdmin(input);
+	}
+
+	// updatePropertyByAdmin
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async updatePropertyByAdmin(@Args('input') input: PropertyUpdate): Promise<Property> {
+		console.log('Mutation: updatePropertyByAdmin');
+		return await this.propertyService.updatePropertyByAdmin(input);
 	}
 }
