be4925b685f89d52816d0be649f88b184b8bb85c
SUNNATULLO <sunnatullokenjaev8@gmail.com>
2025-10-05 13:45:41 +0900

    feat: configure boardArticle module and integrate boardArticle DTO

---
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { BoardArticle } from '../../libs/dto/board-article/board-article';
+
+@Injectable()
+export class BoardArticleService {
+    constructor(
+        @InjectModel('BoardArticle') private readonly boardArticleModule: Model<BoardArticle>) {}
+}
